a6693987cb8d04c199187554621d54c150234db7
Mohamed Ashfi Uzama <122688772+AshfiM@users.noreply.github.com>
2023-09-15 14:30:19 +0500

    removed my API key

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 
 export default function MyApp() {
     //OpenWeatherAPIKey 
-    const APIKEY = "1d8f9dd146c6fac9dc4e77a3c9a3c723"
+    const APIKEY = ""
   return (<>
     <h1>Weather App</h1>
     <SearchBar APIKEY={APIKEY}/>
